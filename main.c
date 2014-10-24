14ecd7a77c8ebcc9dd27334bc65e76919017574c
Sapein <Chandler182@gmail.com>
2014-10-23 22:37:14 -0500

    Minor Bugfix
    This is a minor bugfix in which the program would close out immediately, I
    have now implimented getchar() to wait instead, hopefully this will
    prevent it from shutting down randomly. I'll do some code cleanup tomorrow
    though.

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
  *
  *    Description:  A small little calculator Program I made
  *
- *        Version:  1.0
+ *        Version:  1.1
  *        Created:  10/23/2014 9:24:02 PM
  *       Revision:  none
  *       Compiler:  gcc
@@ -22,7 +22,7 @@ int main(){
 	int choice; //This is simply for the user's choice
 	int firstNumber; //This is for the First inputted Number to operate with
 	int secondNumber; //This is the second number for the program that the user defines
-
+	
 	//This is the introducetion section of the program, this is also where users enter the values that they need to
 	printf("Hello and welcome to Sapein's Simple Calculator Program!\n");
 	printf("Please enter one of the below options to get started!\n");
@@ -39,18 +39,24 @@ int main(){
 	//This is where we decide what to do with the inputs. Wrong input is saved for later
 	if(choice == 1){
 		printf("%d * %d = %d", firstNumber, secondNumber, firstNumber*secondNumber);
+		char wait = getchar();
 	}
 	else if(choice == 2){
 		printf("%d / %d = %d", firstNumber, secondNumber, firstNumber/secondNumber);
+		char wait = getchar();
 	}
 	else if(choice == 3){
 		printf("%d + %d = %d", firstNumber, secondNumber, firstNumber+secondNumber);
+		char wait = getchar();
 	}
 	else if(choice == 4){
 		printf("%d - %d = %d", firstNumber, secondNumber, firstNumber-secondNumber);
+		char wait = getchar();
 	}
 	else{
 		printf("Wrong Input");
+		char wait = getchar();
 	}
+	char wait = getchar();
 	return 0;
 }
