9693b76a06f7e68757dbbf49c2bd7a670a45d5bd
Sapein <Chandler182@gmail.com>
2014-10-23 22:02:01 -0500

    First commit!
    This is my first commit. There shouldn't be too many changes to the
    program, but always make sure you have the latest version!

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a simple calculator program that I've made, it's also written in C.
+
+Please note that I am learning C, this is essentially something that I"m working on as I learn C.
---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  A small little calculator Program I made
+ *
+ *        Version:  1.0
+ *        Created:  10/23/2014 9:24:02 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author: Sapein (CC), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+int main(){
+	int choice; //This is simply for the user's choice
+	int firstNumber; //This is for the First inputted Number to operate with
+	int secondNumber; //This is the second number for the program that the user defines
+
+	//This is the introducetion section of the program, this is also where users enter the values that they need to
+	printf("Hello and welcome to Sapein's Simple Calculator Program!\n");
+	printf("Please enter one of the below options to get started!\n");
+	printf("1.Multipulcation\n");
+	printf("2.Division\n");
+	printf("3.Addition\n");
+	printf("4.Subtraction\n");
+	scanf(" %d", &choice);
+	printf("\nFirst Number: ");
+	scanf(" %d", &firstNumber);
+	printf("\nSecond Number:");
+	scanf(" %d", &secondNumber);
+
+	//This is where we decide what to do with the inputs. Wrong input is saved for later
+	if(choice == 1){
+		printf("%d * %d = %d", firstNumber, secondNumber, firstNumber*secondNumber);
+	}
+	else if(choice == 2){
+		printf("%d / %d = %d", firstNumber, secondNumber, firstNumber/secondNumber);
+	}
+	else if(choice == 3){
+		printf("%d + %d = %d", firstNumber, secondNumber, firstNumber+secondNumber);
+	}
+	else if(choice == 4){
+		printf("%d - %d = %d", firstNumber, secondNumber, firstNumber-secondNumber);
+	}
+	else{
+		printf("Wrong Input");
+	}
+	return 0;
+}
